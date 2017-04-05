95ed4139195912cde3a1eebaa9e9d04821509ed6
Flavia Penim <flaviapenim@gmail.com>
2017-04-05 11:30:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Validator Prototype
+This is prototype version of the meta-data validator for the USI project.
+
+##License
+
+See the LICENSE file for license rights and limitations (MIT).
---
+++ src/main/java/uk/ac/ebi/subs/ValidatorApp.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.subs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by flaviapenim on 05/04/2017.
+ */
+@SpringBootApplication
+public class ValidatorApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatorApp.class, args);
+    }
+}
