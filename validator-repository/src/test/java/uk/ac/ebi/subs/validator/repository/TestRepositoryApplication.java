4b64a044b7095a746e35b32cc09a073e1ddfa808
Karoly Erdos <karoly@ebi.ac.uk>
2017-05-04 12:06:12 +0100

    Add MongoDB repository configuration

---
+++ validator-repository/src/test/java/uk/ac/ebi/subs/validator/repository/TestRepositoryApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.subs.validator.repository;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepositoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestRepositoryApplication.class, args);
+    }
+}
